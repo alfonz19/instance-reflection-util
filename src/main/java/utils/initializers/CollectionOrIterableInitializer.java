8c93ba50bf1158778509fd7a5c9acd049c3900a8
Martin Mucha <alfonz19@gmail.com>
2018-03-18 20:01:13 +0100

    Replaced delegation in CollectinOrIterableInitializer with
    direct creating of ArrayList or HashSet. Reason for that is
    unability to properly calculate new genericType which we should pass
    to delegate
    
    Signed-off-by: Martin Mucha <alfonz19@gmail.com>

--- src/main/java/utils/initializers/CollectionOrIterableInitializer.java
+++ src/main/java/utils/initializers/CollectionOrIterableInitializer.java
@@ -1,24 +1,35 @@
 package utils.initializers;
 
 import java.lang.reflect.Type;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import utils.traverser.ClassTreeTraverserContext;
+import utils.GenericTypeUtil;
 
-public class CollectionOrIterableInitializer extends RandomInitializer {
+public class CollectionOrIterableInitializer extends ArrayLikeInitializerParent {
+
     @Override
     public boolean canProvideValueFor(Class<?> type, Type genericType) {
         return Collection.class.isAssignableFrom(type) || Iterable.class.isAssignableFrom(type);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public Object getValue(Class<?> type, Type genericType, ClassTreeTraverserContext context) {
-        List<Class<? extends Collection>> delegateTo = Arrays.asList(List.class, Set.class);
-        Class<? extends Collection> delegateClass = delegateTo.get(random.nextInt(delegateTo.size()));
-        return getInitializers().getSoleInitializer(delegateClass, genericType)
-                .getValue(delegateClass, genericType, context);//TODO MM: error, you should fix genericType as well.
+    protected Object instantiateCollection(Class<?> type, Type typeOfElements, List items) {
+
+        //collection will be either list or set.
+        boolean useList = random.nextBoolean();
+        if (useList) {
+            return new ArrayList(items);
+        } else {
+            return new HashSet<>(items);
+        }
+    }
+
+    @Override
+    protected Type getTypeOfElements(Type genericType) {
+        return GenericTypeUtil.typeOfListSetElements(genericType);
     }
 }
